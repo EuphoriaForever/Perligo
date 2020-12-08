afedc5b63c0ea52d2e81b1866f0c4a31f0ee72dc
Ismael Francisco <smiiiileeeey11@gmail.com>
2020-12-08 19:32:34 +0800

    first version

---
+++ app.js
@@ -0,0 +1,195 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const mysql = require('mysql')
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: true})
+const { worker } = require("cluster");
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret: 'fletcher', saveUninitialized: true, resave: true}))
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'perligo'
+})
+
+var sess
+
+app.get("/", (req, res)=>{
+    sess = req.session;
+    if(sess.loggedin){
+        res.render('convert', {loggedin:true});
+    } else {
+        res.render('convert', {loggedin:false});
+    }
+});
+
+app.get("/login", (req, res)=>{
+    res.render('login', {loggedin: '', error: false});
+});
+
+app.get("/register", (req, res)=>{
+    res.render('register', {loggedin: ''});
+});
+
+app.post("/register", (req, res)=>{
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(req.body.password, salt); 
+    let qry = 'INSERT INTO users(username, email, password) VALUES("'+req.body.username+'", "'+req.body.email+'", "'+hash+'")';
+    connection.query(qry, (err) =>{
+        if (err) throw err;
+    });
+    res.redirect("/login");
+})
+
+app.post("/login", (req, res)=>{
+    sess = req.session;
+    let qry = 'SELECT * FROM users WHERE username="'+req.body.username+'"';
+    connection.query(qry, (err, response) =>{
+        if(err){
+            throw err;
+        } else if(response.length > 0 && bcrypt.compareSync(req.body.password, response[0]['password'])){
+            sess.loggedin = true;
+            res.redirect('/');
+        } else {
+            res.render('login', {loggedin: '', error: true});
+        }
+    });
+});
+
+app.get("/filesharing", (req, res)=>{
+    sess = req.session;
+    if(sess.loggedin){
+        res.render('filesharing', {loggedin: true});
+    } else {
+        res.render('filesharing', {loggedin: false});
+    }
+});
+
+app.get("/convert", (req, res)=>{
+    sess = req.session;
+    if(sess.loggedin){
+        res.render('convert', {loggedin: true});
+    } else {
+        res.render('convert', {loggedin: false});
+    }
+});
+
+app.get('/logout',(req,res) => {
+    sess = req.session;
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        res.redirect('/login');
+    });
+});
+
+app.post("/convert", (req, res)=>{
+    if(req.body.type != "mp3"){
+        // do text
+        res.redirect('convert');
+        const fs = require('fs');
+        const sdk = require("microsoft-cognitiveservices-speech-sdk");
+        const speechConfig = sdk.SpeechConfig.fromSubscription("0619c56b48914ba3a1601c59eeae3959", "southeastasia");
+
+        function fromFile() {
+            let pushStream = sdk.AudioInputStream.createPushStream();
+
+            fs.createReadStream(req.body.url).on('data', function(arrayBuffer) {
+                pushStream.write(arrayBuffer.slice());
+            }).on('end', function() {
+                pushStream.close();
+            });
+
+            let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+            let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+            recognizer.recognizeOnceAsync(result => {
+                console.log(`RECOGNIZED: Text=${result.text}`);
+                recognizer.close();
+            });
+        }
+        fromFile();
+    }
+    if(req.body.type != "text"){
+        // do mp3
+        console.log(req.body.url)
+        var fs = require('fs'),
+        cloudconvert = new (require('cloudconvert'))('stOAoEwArR56SOS47tt2oiNaplPqgIfEgD2YTKNJac2Cc3jlTME55OlSe02qXZtf');
+        
+        fs.createReadStream(req.body.url)
+        .pipe(cloudconvert.convert({
+            "inputformat": "mp4",
+            "outputformat": "mp3",
+        })).pipe(fs.createWriteStream('output.mp3'))
+        .on('finish', function() {
+            console.log("Conversion has finished!");
+        });
+        console.log("Starting conversion. Please don't quit the webpage until conversion has finished.")
+        
+        res.redirect('convert');
+    }
+});
+
+app.post("/converttext", (req, res)=>{
+    res.redirect('convert');
+    const fs = require('fs');
+    const sdk = require("microsoft-cognitiveservices-speech-sdk");
+    const speechConfig = sdk.SpeechConfig.fromSubscription("0619c56b48914ba3a1601c59eeae3959", "southeastasia");
+
+    function fromFile() {
+    let pushStream = sdk.AudioInputStream.createPushStream();
+
+    fs.createReadStream(req.body.url).on('data', function(arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+    }).on('end', function() {
+        pushStream.close();
+    });
+
+    let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+    let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.recognizeOnceAsync(result => {
+        console.log(`RECOGNIZED: Text=${result.text}`);
+        recognizer.close();
+    });
+    }
+    fromFile();
+});
+
+app.get("/convertMP3", (req, res)=>{
+    res.redirect('convert');
+    var fs = require('fs'),
+    cloudconvert = new (require('cloudconvert'))('stOAoEwArR56SOS47tt2oiNaplPqgIfEgD2YTKNJac2Cc3jlTME55OlSe02qXZtf');
+
+    fs.createReadStream('inputfile.mp4')
+    .pipe(cloudconvert.convert({
+    "inputformat": "mp4",
+    "outputformat": "mp3",
+    }))
+    console.log("Starting conversion. Please don't quit the webpage until conversion has finished.")
+    .pipe(fs.createWriteStream('output.mp3'))
+    .on('finish', function() {
+        console.log("Conversion has finished!");
+    });
+});
+
+app.get("/drive", (req, res)=>{
+    sess = req.session;
+    if(sess.loggedin){
+        res.render('drive', {loggedin: true});
+    } else {
+        res.render('drive', {loggedin: false});
+    }
+});
+
+app.listen(3000);
+console.log('Listening to Port 3000');
---
+++ perligo.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2020 at 11:38 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perligo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(10, 'admin', '', '$2b$10$Zsjub/nhb9RIsjZ5kWnUBuLcXFZz2DhBz6Hx5kL6CK3lL9UM4KC0u'),
+(11, 'test', 'test@gmail.com', '$2b$10$MiEpP5PV.Obh3C.DnOGCdOT.ek6.BD7SRJapydAQyuMiehtotboO6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
